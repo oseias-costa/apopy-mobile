063780cb94f8ca51f2d6ced6692d8b463e08563e
Oséias Costa <oseiasc2@gmail.com>
2023-09-18 19:27:00 +0000

    feat(Suplier): create loadingAction function for loading of the buttom

--- presentation/screens/Supliers/Supliers.tsx
+++ presentation/screens/Supliers/Supliers.tsx
@@ -1,106 +1,156 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { NativeSyntheticEvent, TextInputFocusEventData } from "react-native";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Title from "../../components/Title";
 import { useGetSuplier } from "../../hooks/useGetSupliers";
 import SuplierItem from "./components/SuplierItem";
-import { BottomSheet } from 'react-native-btr';
-import Input from '../../components/Input/Input';
-import CustomButton from '../../components/CustomButton/CustomButton';
-import { updateSuplierUseCase } from '../../../application/suplier.usecase';
-import { useDispatch } from 'react-redux';
-import { updateSuplier } from '../../redux/slice/suplierSlice';
-import ModalDelete from './components/ModalDelete';
+import { BottomSheet } from "react-native-btr";
+import Input from "../../components/Input/Input";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import {
+  createSuplierUseCase,
+  deleteSuplierUseCase,
+  updateSuplierUseCase,
+} from "../../../application/suplier.usecase";
+import { useDispatch } from "react-redux";
+import {
+  createSuplier,
+  removeSuplier,
+  updateSuplier,
+} from "../../redux/slice/suplierSlice";
+import ModalDelete from "./components/ModalDelete";
 
-export default function Supliers(){
-    const { supliers, loading } = useGetSuplier()
-    const [visible, setVisible] = useState(false)
-    const dispatch = useDispatch()
-    const [ suplierState, setSuplierState ] = useState({
-        name: '', _id: '', loading: false, delete: false
-    })
+export default function Supliers() {
+  const { supliers, loading } = useGetSuplier();
+  const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
+  const [suplierState, setSuplierState] = useState({
+    name: "",
+    _id: "",
+    loading: false,
+    delete: false,
+  });
 
-    const updateSuplierItem = async () => {
-        setSuplierState({...suplierState, loading: true})
-        const update = await updateSuplierUseCase(suplierState._id, suplierState.name) 
-      
-        if(update.data.data){
-            dispatch(updateSuplier(update.data.data.updateSuplier))
-            toggleBottomNavigationView()
-            setSuplierState({...suplierState, loading: false})
-        }
+  const loadingAction = (isLoading: boolean) =>
+    isLoading
+      ? setSuplierState({ ...suplierState, loading: true })
+      : setSuplierState({ ...suplierState, loading: false });
+
+  const createSuplierItem = async () => {
+    loadingAction(true);
+    const create = await createSuplierUseCase(suplierState.name);
+
+    if (create.data.data) {
+      dispatch(createSuplier(suplierState));
+      loadingAction(false);
     }
+  };
 
-    const listSupliers = supliers.map(suplier => 
-        <SuplierItem 
-            item={suplier.name} 
-            key={suplier._id} 
+  const updateSuplierItem = async () => {
+    loadingAction(true);
+    const update = await updateSuplierUseCase(
+      suplierState._id,
+      suplierState.name,
+    );
+
+    if (update.data.data) {
+      dispatch(updateSuplier(update.data.data.updateSuplier));
+      toggleBottomNavigationView();
+      loadingAction(false);
+    }
+  };
+
+  const deleteSuplierItem = async () => {
+    loadingAction(true);
+    const deleteSup = await deleteSuplierUseCase(suplierState._id);
+
+    if (deleteSup.data.data) {
+      dispatch(removeSuplier(suplierState));
+      loadingAction(false);
+    }
+  };
+
+  const listSupliers = supliers.map((suplier) => (
+    <SuplierItem
+      item={suplier.name}
+      key={suplier._id}
+      onPress={() => {
+        setSuplierState({
+          ...suplierState,
+          name: suplier.name,
+          _id: suplier._id,
+        });
+        setVisible(!visible);
+      }}
+    />
+  ));
+
+  const toggleBottomNavigationView = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <ModalDelete
+        visible={suplierState.delete}
+        onPress={deleteSuplierItem}
+        cancelAction={() => setSuplierState({ ...suplierState, delete: false })}
+      />
+      <Title>Supliers</Title>
+      <View>{listSupliers}</View>
+      <BottomSheet
+        visible={visible}
+        onBackButtonPress={toggleBottomNavigationView}
+        onBackdropPress={toggleBottomNavigationView}
+      >
+        <View style={styles.modalize}>
+          <View style={styles.lineContainer}>
+            <View style={styles.line} />
+          </View>
+          <Input
+            value={suplierState.name}
+            onChange={(e: NativeSyntheticEvent<TextInputFocusEventData>) =>
+              setSuplierState({ ...suplierState, name: e.nativeEvent.text })
+            }
+            placeholder="Digite um fornecedor"
+          />
+          <CustomButton
+            text="Editar"
+            disabled={suplierState.loading}
+            onPress={updateSuplierItem}
+          />
+          <CustomButton
+            text="Excluir"
+            disabled={false}
             onPress={() => {
-                setSuplierState({...suplierState, name: suplier.name, _id: suplier._id })
-                setVisible(!visible)}}
-        />)
-
-        const toggleBottomNavigationView = () => {
-            setVisible(!visible);
-          };
-        
-
-    return(
-        <ScrollView style={styles.container}>
-            <ModalDelete visible={suplierState.delete} onPress={() => setSuplierState({...suplierState, delete: false})} />
-            <Title>Supliers</Title>
-            <View>{listSupliers}</View>
-            <BottomSheet visible={visible}
-                onBackButtonPress={toggleBottomNavigationView}
-                onBackdropPress={toggleBottomNavigationView}
-            >
-                <View style={styles.modalize}>
-                    <View style={styles.lineContainer}>
-                        <View style={styles.line} />
-                    </View>
-                    <Input 
-                        value={suplierState.name} 
-                        onChange={(e: NativeSyntheticEvent<TextInputFocusEventData>) => 
-                            setSuplierState({...suplierState, name: e.nativeEvent.text})} 
-                        placeholder="Digite um fornecedor"
-                    />
-                    <CustomButton 
-                        text='Editar' 
-                        disabled={suplierState.loading} 
-                        onPress={updateSuplierItem}
-                    />
-                    <CustomButton 
-                        text='Excluir' 
-                        disabled={false} 
-                        onPress={()=> {
-                            toggleBottomNavigationView()
-                            setSuplierState({...suplierState, delete: true})
-                        }}
-                    />
-                </View>
-            </BottomSheet>
-        </ScrollView>
-    )
+              toggleBottomNavigationView();
+              setSuplierState({ ...suplierState, delete: true });
+            }}
+          />
+        </View>
+      </BottomSheet>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#FCFCFC',
-    },
-    modalize: {
-        backgroundColor: '#FCFCFC',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
-    },
-    lineContainer: {
-        marginVertical: 10,
-        alignItems: 'center',
-        paddingBottom: 20
-    },
-    line: {
-        width: 50,
-        height: 4,
-        backgroundColor: 'black',
-        borderRadius: 20
-    }
-})
+  container: {
+    backgroundColor: "#FCFCFC",
+  },
+  modalize: {
+    backgroundColor: "#FCFCFC",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  lineContainer: {
+    marginVertical: 10,
+    alignItems: "center",
+    paddingBottom: 20,
+  },
+  line: {
+    width: 50,
+    height: 4,
+    backgroundColor: "black",
+    borderRadius: 20,
+  },
+});
--- presentation/screens/Supliers/components/ModalDelete.tsx
+++ presentation/screens/Supliers/components/ModalDelete.tsx
@@ -1,18 +1,35 @@
-import { Button, GestureResponderEvent, View } from "react-native";
+import { Button, GestureResponderEvent, StyleSheet, View } from "react-native";
 import { Modal, Text } from "react-native";
 
-export default function ModalDelete({visible, onPress}: {visible: boolean, onPress: (event: GestureResponderEvent) => void}){
-    return(
-        <Modal 
-            animationType="fade"
-            visible={visible}
-            style={{ backgroundColor: 'black'}}
-            transparent={true}
-        >
-            <View style={{ height: 200, width: 200, backgroundColor: 'red'}}>
-                <Text>Teste</Text>
-                <Button onPress={onPress} title='fechar' />
-            </View>
-        </Modal>
-    )
+export default function ModalDelete({
+  visible,
+  onPress,
+  cancelAction,
+}: {
+  visible: boolean;
+  onPress: (event: GestureResponderEvent) => void;
+  cancelAction: (event: GestureResponderEvent) => void;
+}) {
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType="fade"
+        visible={visible}
+        style={{ backgroundColor: "black" }}
+        transparent={true}
+      >
+        <View style={{ height: 200, width: 200, backgroundColor: "red" }}>
+          <Text>Teste</Text>
+          <Button onPress={cancelAction} title="Cancelar" />
+          <Button onPress={onPress} title="Excluir" />
+        </View>
+      </Modal>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(0,0,0, 0.5)",
+  },
+});
